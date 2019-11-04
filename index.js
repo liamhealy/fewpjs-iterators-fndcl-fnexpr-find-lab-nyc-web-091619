7cc9654373d6abf3c2a5c3441cdcfb9f354f2bd8
Liam <liam.healy1@marist.edu>
2019-11-04 14:41:31 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (games) => {
+  let winner = games.find(obj => obj.result === "W")
+  return winner ? winner.year : undefined
+    // if (obj.result === "W") {
+    //   return obj.year;
+    // }
+  // })
+}
